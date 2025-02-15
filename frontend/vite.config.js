2ecdc9edeea13b880662ee44b0bd00f2b2458ee7
sathiya202 <priyamsathiya622@gmail.com>
2025-02-15 23:01:11 +0530

    con

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server:{port:5173}
+// })
 export default defineConfig({
-  plugins: [react()],
-  server:{port:5173}
-})
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://full-stack-mern-856n.onrender.com",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
