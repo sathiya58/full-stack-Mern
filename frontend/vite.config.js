515d150e6684224f2955f9a47605c690bcd782a8
sathiya202 <priyamsathiya622@gmail.com>
2025-02-15 23:04:56 +0530

    vit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,9 @@
 import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react'
 
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   server:{port:5173}
-// })
+// https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://full-stack-mern-856n.onrender.com",
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
-});
+  plugins: [react()],
+  server:{port:5173}
+})
+
